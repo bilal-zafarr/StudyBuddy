c688a48a074882c00ab03b0f5110d3e725917969
Bilal Zafar <bilalzaafar@gmail.com>
2022-08-03 21:08:20 +0500

    sync

--- base/views.py
+++ base/views.py
@@ -3,10 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {"id": 1, "name": "Not what you think"},
+    {"id": 2, "name": "Oh nameee?"},
+    {"id": 3, "name": "You really want name?"},
+]
+
 
 def home(request):
-    return render(request, "home.html")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request):
-    return render(request, "room.html")
+def room(request, pk):
+    room = None
+    for r in rooms:
+        if r["id"] == int(pk):
+            room = r
+    context = {"room": room}
+    return render(request, "base/room.html", context)
